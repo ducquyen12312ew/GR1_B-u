f9f8e712745520dc94fc9d3d45fced19dc0c016a
Hoàng Thái Dương_20225821 <thaiduong040404@gmail.com>
2025-07-05 16:36:13 +0700

    m

--- reactjs/src/services/chatbotAxios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const chatbotAxios = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
-  timeout: 10000,
-});
-
-// Không có interceptor nào cả
-export default chatbotAxios;
